f3e734b8a7a0f37caf1b2b98a1daa3a69e9222ed
rvanfleet <ryan.vanfleet@outlook.com>
2015-05-17 15:34:38 -0400

    Create cm_mongodb.py

---
+++ cm_mongodb.py
@@ -0,0 +1,102 @@
+import subprocess
+import pymongo
+from pymongo import MongoClient
+
+class db:
+
+    database = None
+    jobs = None
+
+    def startMongo(self, db_path="/data/db/", port="27017", log_path="/var/log/mongod"):
+
+        #Remove dbPath and create an empty one
+        subprocess.call(["mkdir", "/data"])
+        subprocess.call(["rm", "-r", db_path])
+        subprocess.call(["mkdir", db_path])
+
+        #Deploy the mongoDB
+        subprocess.call(["mongod", "--dbpath", db_path, "--port", port, "--fork", "--logpath", log_path])
+
+        print "MongoDB has been deployed at path " + db_path + " on port " + port + " with log " + log_path
+
+    def stopMongo(self):
+
+        subprocess.call(["mongod", "--shutdown"])
+
+        print "MongoDB has stopped"
+
+    def connect(self, db_name):
+
+        client = MongoClient()
+
+        self.database = client[db_name]
+        self.jobs = self.database.jobs
+
+    def insertJob(self, job_name, input_filename="", output_filename=""):
+
+        if self.database is not None:
+
+            job = {"job_name"        : job_name,
+                   "input_filename"  : input_filename,
+                   "output_filename" : output_filename,
+                   }
+
+            job_id = self.jobs.insert_one(job).inserted_id
+
+            return job_id
+
+        else:
+
+            print "Please connect to a database before running this function"
+            return -1
+
+    def findJobs(self, key_name="", value=""):
+
+        if self.database is not None:
+
+            #Return all jobs if not given search parameters
+            if key_name == "" and value == "":
+                return self.jobs.find()
+
+            #Return all jobs given the search parameters
+            else:
+                return self.jobs.find({key_name: value})
+
+        else:
+
+            print "Please connect to a database before running this function"
+            return -1
+
+    def deleteJobs(self, key_name="", value=""):
+
+        if self.database is not None:
+
+            #Delete all jobs if no key name or value has been given
+            if key_name == "" and value == "":
+                self.jobs.remove()
+
+            #Delete only records from a query result if key name and value have been given
+            else:
+                self.jobs.remove({key_name: value})
+
+        else:
+
+            print "Please connect to a database before running this function"
+            return -1
+
+    def numJobs(self, key_name="", value=""):
+
+        if self.database is not None:
+
+            #Return the count of all jobs if not given search parameters
+            if key_name == "" and value == "":
+                return self.jobs.count()
+
+            #Return number of results of the query given the search parameters
+            else:
+                return self.jobs.find(({key_name: value})).count()
+
+        else:
+
+            print "Please connect to a database before running this function"
+            return -1
